a2339f1057116e126a938b9bce09ec5f595df5c8
Bruno Lottero <brunoelottero@gmail.com>
2016-12-13 23:06:13 -0300

    matrices to column major order
    lookAt implemented
    hw1 finihed

--- hw1-linuxosx/Transform.cpp
+++ hw1-linuxosx/Transform.cpp
@@ -20,15 +20,15 @@ mat3 Transform::rotate(const float degrees, const vec3& axis) {
   const float z = axis.z;
 
   resulting_matrix[0][0] = x * x * one_minus_cosine + cosine;
-  resulting_matrix[0][1] = x * y * one_minus_cosine - z * sine;
-  resulting_matrix[0][2] = x * z * one_minus_cosine + y * sine;
+  resulting_matrix[1][0] = x * y * one_minus_cosine - z * sine;
+  resulting_matrix[2][0] = x * z * one_minus_cosine + y * sine;
 
-  resulting_matrix[1][0] = x * y * one_minus_cosine + z * sine;
+  resulting_matrix[0][1] = x * y * one_minus_cosine + z * sine;
   resulting_matrix[1][1] = y * y * one_minus_cosine + cosine;
-  resulting_matrix[1][2] = y * z * one_minus_cosine - x * sine;
+  resulting_matrix[2][1] = y * z * one_minus_cosine - x * sine;
 
-  resulting_matrix[2][0] = x * z * one_minus_cosine - y * sine;
-  resulting_matrix[2][1] = y * z * one_minus_cosine + x * sine;
+  resulting_matrix[0][2] = x * z * one_minus_cosine - y * sine;
+  resulting_matrix[1][2] = y * z * one_minus_cosine + x * sine;
   resulting_matrix[2][2] = z * z * one_minus_cosine + cosine;
 
   return resulting_matrix;
@@ -36,7 +36,7 @@ mat3 Transform::rotate(const float degrees, const vec3& axis) {
 
 // Transforms the camera left around the "crystal ball" interface
 void Transform::left(float degrees, vec3& eye, vec3& up) {
-  eye = eye * rotate(degrees, glm::normalize(up));
+  eye = eye * rotate(-degrees, glm::normalize(up));
 
   printf("Coordinates: %.2f, %.2f, %.2f; distance: %.2f\n", eye.x, eye.y, eye.z, sqrt(pow(eye.x, 2) + pow(eye.y, 2) + pow(eye.z, 2)));
 }
@@ -45,7 +45,7 @@ void Transform::left(float degrees, vec3& eye, vec3& up) {
 void Transform::up(float degrees, vec3& eye, vec3& up) {
 
   vec3 w = glm::normalize(glm::cross(eye, up));
-  mat3 rotation_matrix = rotate(degrees, w);
+  mat3 rotation_matrix = rotate(-degrees, w);
   eye = eye * rotation_matrix;
   up = up * rotation_matrix;
 
@@ -54,10 +54,31 @@ void Transform::up(float degrees, vec3& eye, vec3& up) {
 
 // Your implementation of the glm::lookAt matrix
 mat4 Transform::lookAt(vec3 eye, vec3 up) {
-  // YOUR CODE FOR HW1 HERE
 
-  // You will change this return call
-  return mat4();
+  mat4 resulting_matrix(1.0f);
+  mat4 eye_matrix(1.0f);
+
+  vec3 w = glm::normalize(eye);
+  vec3 u = glm::normalize(glm::cross(up, w));
+  vec3 v = glm::cross(w, u);
+
+  resulting_matrix[0][0] = u.x;
+  resulting_matrix[1][0] = u.y;
+  resulting_matrix[2][0] = u.z;
+
+  resulting_matrix[0][1] = v.x;
+  resulting_matrix[1][1] = v.y;
+  resulting_matrix[2][1] = v.z;
+
+  resulting_matrix[0][2] = w.x;
+  resulting_matrix[1][2] = w.y;
+  resulting_matrix[2][2] = w.z;
+
+  resulting_matrix[3][0] = glm::dot(u, -eye);
+  resulting_matrix[3][1] = glm::dot(v, -eye);
+  resulting_matrix[3][2] = glm::dot(w, -eye);
+
+  return resulting_matrix;
 }
 
 Transform::Transform()
